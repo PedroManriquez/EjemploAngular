d63445171c32d1b374af3e881d2be4171fb7c58d
Ricardo Carrasco Cursach <rccursach@gmail.com>
2016-09-22 15:11:10 -0300

    tabs a espacios, injección de scope en controlador y cambio de nombre directivas

--- app/directives.js
+++ app/directives.js
@@ -1,17 +1,21 @@
+'use strict';
+
 (function () {
-	var direc = angular.module('directivas-ejemplo',[]);
-	direc.directive('miPanel',function(){
-		return {
-			restric : 'E',
-			templateUrl : 'app/views/panel.html'
-			
-		}
-	});
-	direc.directive('miPanel',function(){
-		return {
-			restric : 'A',
-			templateUrl : 'app/views/panel.html'
-			
-		}
-	});	
+  var direc = angular.module('directivas-ejemplo',[]);
+
+  direc.directive('miPanel1',function(){
+    return {
+      restric : 'A',
+      templateUrl : 'app/views/panel.html'
+      
+    }
+  });
+  
+  direc.directive('miPanel2',function(){
+    return {
+      restric : 'E',
+      templateUrl : 'app/views/panel.html'
+      
+    }
+  }); 
 })();
